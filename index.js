77ebea80abce46c57d08fdd71ee564edffc72ebf
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2024-08-08 16:43:37 +0100

    added buttons (unwired)

---
+++ index.js
@@ -0,0 +1,23 @@
+//declaring the three parts of a calculation
+let firstNumber = 0
+let secondNumber = 0
+let operator = ''
+
+//creating the 4 different operations
+function addition(a, b) {
+    return a + b
+}
+function subtraction(a, b) {
+    return a - b
+}
+function multiplication(a, b) {
+    return a * b
+}
+function division(a, b) {
+    return a / b
+}
+
+//creating the function to use the operations
+function operate(first, second, operation) {
+    
+}
