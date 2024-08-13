dc789a8b7590f2a2830e2557860326140b21bb3b
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2024-08-13 15:56:14 +0100

    cannot make the function onclick work for buttons

--- index.js
+++ index.js
@@ -20,8 +20,27 @@ function division(a, b) {
 
 //creating the function to use the operations
 function operate(first, second, operation) {
-
+    switch (operation) {
+        case '+':
+            return addition(first, second)
+            break
+        case '-':
+            return subtraction(first, second)
+            break
+        case '*':
+            return multiplication(first, second)
+            break
+        case '/':
+            return division(first, second)
+            break
+    }
 }
 
 //displaying the current number
 const calculatorDisplay = document.querySelector('.display')
+
+//adding event listeners to all of the number buttons
+const numberButtons = document.querySelectorAll('.number')
+numberButtons.forEach((currentNumberButton) => {
+    currentNumberButton.addEventListener('click', numberOnClick(currentNumberButton))
+})
