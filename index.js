afddec8cac1ae63e62c097ed43d0f093cbb066a3
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2024-08-14 16:56:20 +0100

    added functionality to all buttons

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function operate(first, second, operation) {
     }
 }
 
-//displaying the current number
+//declaring the calculator display node
 const calculatorDisplay = document.querySelector('.display')
 
 //adding event listeners to all of the number buttons
@@ -46,4 +46,34 @@ numberButtons.forEach((currentNumberButton) => {
         displayValue = displayValue + currentNumberButton.textContent
         calculatorDisplay.textContent = displayValue
     })
+})
+
+//adding event listeners for the four operators
+const operatorButtons = document.querySelectorAll('.operator')
+operatorButtons.forEach((currentOperatorButton) => {
+    currentOperatorButton.addEventListener('click', () => {
+        firstNumber = displayValue
+        operator = currentOperatorButton.textContent
+        displayValue = ''
+        calculatorDisplay.textContent = displayValue
+    })
+})
+
+//making the equals button function
+const equalsButton = document.querySelector('.equals')
+equalsButton.addEventListener('click', () => {
+    secondNumber = displayValue
+    firstNumber = operate(firstNumber, secondNumber, operator)
+    displayValue = firstNumber
+    calculatorDisplay.textContent = displayValue
+})
+
+//adding functionality to the clear button
+const clearButton = document.querySelector('.clearButton')
+clearButton.addEventListener('click', () => {
+    firstNumber = 0
+    secondNumber = 0
+    operator = ''
+    displayValue = ''
+    calculatorDisplay.textContent = ''
 })
